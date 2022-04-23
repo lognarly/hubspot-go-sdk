eba38baa028c5f011fdf7b6093d8a3e1c8b45abc
Logan Rohloff <logan@rapdev.io>
2022-04-23 19:42:03 -0400

    association definition functions

--- associations.go
+++ associations.go
@@ -9,6 +9,9 @@ type Associations interface {
 	List(options *AssociationListOptions, query *AssociationListQuery) (*AssociationList, error)
 	Create(options *[]AssociationCreateOptions, query *AssociationCreateOrDeleteQuery) (*AssociationCreateOutput, error)
 	Delete(query *AssociationCreateOrDeleteQuery) (error)
+	ReadDefinition(query *AssociationDefinitionQuery) (*AssociationReadDefinition, error)
+	CreateDefinition(options *AssociationCreateDefinitionOptions, query *AssociationDefinitionQuery) (*AssociationDefinition, error)
+	UpdateDefinition(options *AssociationUpdateDefinitionOptions, query *AssociationUpdateDefinitionQuery) (error)
 }
 
 type associations struct {
@@ -69,6 +72,37 @@ type AssociationCreateOutput struct {
 	Labels           []string `json:"labels"`
 }
 
+type AssociationDefinitionQuery struct {
+	FromObjectType string
+	ToObjectType   string
+}
+
+type AssociationDefinitionOutput struct {
+	Results []AssociationDefinition `json:"results"`
+}
+
+type AssociationDefinition struct {
+	Category HubspotAssociationCategory `json:"category"`
+	TypeId   int                        `json:"typeId"`
+	Label    string                     `json:"label"`
+}
+
+type AssociationCreateDefinitionOptions struct {
+	Label string `json:"label"`
+	Name  string `json:"name"`
+}
+
+type AssociationUpdateDefinitionOptions struct {
+	Label string `json:"label"`
+	TypeId int64 `json:"associationTypeId"`
+}
+
+type AssocationDeleteDefinitionQuery struct {
+	FromObjectType string 
+	ToObjectType   string 
+	TypeId         int64  
+}
+
 func (a *associations) List(options *AssociationListOptions, query *AssociationListQuery) (*AssociationList, error) {
 	u := fmt.Sprintf("crm/v4/objects/%s/%s/associations/%s", query.FromObjectType, strconv.FormatInt(query.FromObjectId, 10), query.ToObjectType)
 	req, err := a.client.newHttpRequest("GET", u, options)
@@ -80,7 +114,7 @@ func (a *associations) List(options *AssociationListOptions, query *AssociationL
 
 	err = a.client.do(req, al)
 	if err != nil {
-		return nil, fmt.Errorf("client.association.List(): do()(): %v", err)
+		return nil, fmt.Errorf("client.association.List(): do(): %v", err)
 	}
 
 	return al, nil
@@ -97,7 +131,7 @@ func (a *associations) Create(options *[]AssociationCreateOptions, query *Associ
 
 	err = a.client.do(req, aco)
 	if err != nil {
-		return nil, fmt.Errorf("client.association.Create(): do()(): %v", err)
+		return nil, fmt.Errorf("client.association.Create(): do(): %v", err)
 	}
 
 	return aco, nil
@@ -108,6 +142,60 @@ func (a *associations) Delete(query *AssociationCreateOrDeleteQuery) (error) {
 	req, err := a.client.newHttpRequest("DELETE", u, nil)
 	if err != nil {
 		return fmt.Errorf("client.association.Archive(): newHttpRequest(): %v", err)
+	}
+
+	return a.client.do(req, nil)
+}
+
+func (a *associations) ReadDefinition(query *AssociationDefinitionQuery) (*AssociationDefinitionOutput, error) {
+	u := fmt.Sprintf("/crm/v4/associations/%s/%s/labels", query.FromObjectType, query.ToObjectType)
+	req, err := a.client.newHttpRequest("GET", u, nil)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.ReadDefinition(): newHttpRequest(): %v", err)
+	}
+
+	ard := &AssociationDefinitionOutput{}
+
+	err = a.client.do(req, ard)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.ReadDefinition(): do(): %v", err)
+	}
+
+	return ard, nil
+}
+
+func (a *associations) CreateDefinition(options *AssociationCreateDefinitionOptions, query *AssociationDefinitionQuery) (*AssociationDefinitionOutput, error) {
+	u := fmt.Sprintf("/crm/v4/associations/%s/%s/labels", query.FromObjectType, query.ToObjectType)
+	req, err := a.client.newHttpRequest("POST", u, options)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.CreateDefinition(): newHttpRequest(): %v", err)
+	}
+
+	ard := &AssociationDefinitionOutput{}
+
+	err = a.client.do(req, ard)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.CreateDefinition(): do(): %v", err)
+	}
+
+	return ard, nil
+}
+
+func (a *associations) UpdateDefinition(options *AssociationUpdateDefinitionOptions, query *AssociationUpdateDefinitionQuery) (error) {
+	u := fmt.Sprintf("/crm/v4/associations/%s/%s/labels", query.FromObjectType, query.ToObjectType)
+	req, err := a.client.newHttpRequest("PUT", u, options)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.UpdateDefinition(): newHttpRequest(): %v", err)
+	}
+
+	return a.client.do(req, nil)
+}
+
+func (a *associations) DeleteDefinition(query *AssocationDeleteDefinitionQuery) (error) {
+	u := fmt.Sprintf("/crm/v4/associations/%s/%s/labels/%s", query.FromObjectType, query.ToObjectType, strconv.FormatInt(query.TypeId, 10))
+	req, err := a.client.newHttpRequest("PUT", u, options)
+	if err != nil {
+		return nil, fmt.Errorf("client.association.UpdateDefinition(): newHttpRequest(): %v", err)
 	}
 
 	return a.client.do(req, nil)
