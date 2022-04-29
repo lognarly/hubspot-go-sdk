2cae6ba39985c0885fcd3e353a51a3925cab42dc
Logan Rohloff <logan@rapdev.io>
2022-04-29 10:20:06 -0400

    rename file, string everything because of the API

--- crm/line_items_properties.go
+++ crm/line_items_properties.go
@@ -1,59 +1,59 @@
 package crm
 
 type LineItemProperties struct {
-	Amount                                float64 `json:"amount,omitempty"`
+	Amount                                string `json:"amount,omitempty"`
 	Createdate                            string  `json:"createdate,omitempty"`
 	Description                           string  `json:"description,omitempty"`
-	Discount                              float64 `json:"discount,omitempty"`
-	HsAcv                                 int64   `json:"hs_acv,omitempty"`
+	Discount                              string `json:"discount,omitempty"`
+	HsAcv                                 string   `json:"hs_acv,omitempty"`
 	HsAllAccessibleTeamIds                string  `json:"hs_all_accessible_team_ids,omitempty"`
 	HsAllAssignedBusinessUnitIds          string  `json:"hs_all_assigned_business_unit_ids,omitempty"`
 	HsAllOwnerIds                         string  `json:"hs_all_owner_ids,omitempty"`
 	HsAllTeamIds                          string  `json:"hs_all_team_ids,omitempty"`
 	HsAllowBuyerSelectedQuantity          string  `json:"hs_allow_buyer_selected_quantity,omitempty"`
-	HsArr                                 int64   `json:"hs_arr,omitempty"`
-	HsCostOfGoodsSold                     int64   `json:"hs_cost_of_goods_sold,omitempty"`
+	HsArr                                 string   `json:"hs_arr,omitempty"`
+	HsCostOfGoodsSold                     string   `json:"hs_cost_of_goods_sold,omitempty"`
 	HsCreatedByUserId                     string  `json:"hs_created_by_user_id,omitempty"`
 	HsCreatedate                          string  `json:"hs_createdate,omitempty"`
-	HsDiscountPercentage                  float64 `json:"hs_discount_percentage,omitempty"`
+	HsDiscountPercentage                  string `json:"hs_discount_percentage,omitempty"`
 	HsExternalId                          string  `json:"hs_external_id,omitempty"`
 	HsImages                              string  `json:"hs_images,omitempty"`
 	HsLastmodifieddate                    string  `json:"hs_lastmodifieddate,omitempty"`
 	HsLineItemCurrencyCode                string  `json:"hs_line_item_currency_code,omitempty"`
-	HsMargin                              int64   `json:"hs_margin,omitempty"`
-	HsMarginAcv                           int64   `json:"hs_margin_acv,omitempty"`
-	HsMarginArr                           int64   `json:"hs_margin_arr,omitempty"`
-	HsMarginMrr                           int64   `json:"hs_margin_mrr,omitempty"`
-	HsMarginTcv                           int64   `json:"hs_margin_tcv,omitempty"`
+	HsMargin                              string   `json:"hs_margin,omitempty"`
+	HsMarginAcv                           string   `json:"hs_margin_acv,omitempty"`
+	HsMarginArr                           string   `json:"hs_margin_arr,omitempty"`
+	HsMarginMrr                           string   `json:"hs_margin_mrr,omitempty"`
+	HsMarginTcv                           string   `json:"hs_margin_tcv,omitempty"`
 	HsMergedObjectIds                     string  `json:"hs_merged_object_ids,omitempty"`
-	HsMrr                                 int64   `json:"hs_mrr,omitempty"`
+	HsMrr                                 string   `json:"hs_mrr,omitempty"`
 	HsObjectId                            string  `json:"hs_object_id,omitempty"`
 	HsPositionOnQuote                     string  `json:"hs_position_on_quote,omitempty"`
-	HsPreDiscountAmount                   float64 `json:"hs_pre_discount_amount,omitempty"`
+	HsPreDiscountAmount                   string  `json:"hs_pre_discount_amount,omitempty"`
 	HsProductId                           string  `json:"hs_product_id,omitempty"`
 	HsRecurringBillingEndDate             string  `json:"hs_recurring_billing_end_date,omitempty"`
-	HsRecurringBillingNumberOfPayments    int64   `json:"hs_recurring_billing_int64_of_payments,omitempty"`
+	HsRecurringBillingNumberOfPayments    string   `json:"hs_recurring_billing_string_of_payments,omitempty"`
 	HsRecurringBillingPeriod              string  `json:"hs_recurring_billing_period,omitempty"`
 	HsRecurringBillingStartDate           string  `json:"hs_recurring_billing_start_date,omitempty"`
 	HsRecurringBillingTerms               string  `json:"hs_recurring_billing_terms,omitempty"`
 	HsSku                                 string  `json:"hs_sku,omitempty"`
-	HsSyncAmount                          int64   `json:"hs_sync_amount,omitempty"`
-	HsTcv                                 int64   `json:"hs_tcv,omitempty"`
-	HsTermInMonths                        int64   `json:"hs_term_in_months,omitempty"`
-	HsTotalDiscount                       float64 `json:"hs_total_discount,omitempty"`
+	HsSyncAmount                          string   `json:"hs_sync_amount,omitempty"`
+	HsTcv                                 string   `json:"hs_tcv,omitempty"`
+	HsTermInMonths                        string   `json:"hs_term_in_months,omitempty"`
+	HsTotalDiscount                       string  `json:"hs_total_discount,omitempty"`
 	HsUniqueCreationKey                   string  `json:"hs_unique_creation_key,omitempty"`
 	HsUpdatedByUserId                     string  `json:"hs_updated_by_user_id,omitempty"`
 	HsUrl                                 string  `json:"hs_url,omitempty"`
 	HsUserIdsOfAllNotificationFollowers   string  `json:"hs_user_ids_of_all_notification_followers,omitempty"`
 	HsUserIdsOfAllNotificationUnfollowers string  `json:"hs_user_ids_of_all_notification_unfollowers,omitempty"`
 	HsUserIdsOfAllOwners                  string  `json:"hs_user_ids_of_all_owners,omitempty"`
-	HsVariantId                           int64   `json:"hs_variant_id,omitempty"`
+	HsVariantId                           string  `json:"hs_variant_id,omitempty"`
 	HubspotOwnerAssigneddate              string  `json:"hubspot_owner_assigneddate,omitempty"`
 	HubspotOwnerId                        string  `json:"hubspot_owner_id,omitempty"`
 	HubspotTeamId                         string  `json:"hubspot_team_id,omitempty"`
 	Name                                  string  `json:"name,omitempty"`
-	Price                                 float64 `json:"price,omitempty"`
-	Quantity                              int64   `json:"quantity,omitempty"`
+	Price                                 string  `json:"price,omitempty"`
+	Quantity                              string   `json:"quantity,omitempty"`
 	Recurringbillingfrequency             string  `json:"recurringbillingfrequency,omitempty"`
-	Tax                                   float64 `json:"tax,omitempty"`
+	Tax                                   string `json:"tax,omitempty"`
 }
